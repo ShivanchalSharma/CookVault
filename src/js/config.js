22b5463753c09f39f1b49d1ff5adabcb97f6a2a8
ShivanchalSharma <shivanchalharma77@gmail.com>
2026-07-07 21:56:53 +0530

    Rebrand to CookVault: retro theme, new logo, updated links

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
-export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/'
-export const timeoutSeconds = 10
-export const resultsPerPage = 12
-export const KEY = '552498fd-7ae4-41d8-8c32-51db5ee204fe'
-export const MODAL_CLOSE_M_SEC = 1000
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const timeoutSeconds = 10;
+export const resultsPerPage = 12;
+export const KEY = '552498fd-7ae4-41d8-8c32-51db5ee204fe';
+export const MODAL_CLOSE_M_SEC = 1000;
